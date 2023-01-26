f6366027b89709c200abe692fe0fa73b9ed2260d
Sergei Shabalin <6f98v468t@mozmail.com>
2023-01-26 23:47:58 +0300

    Initial Commit

---
+++ SimpleAnimationApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleAnimationApp
+//
+//  Created by serg on 26.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
